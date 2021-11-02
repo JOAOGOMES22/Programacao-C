8fc756d18aea14f42701be9a31e30b7fee63a7eb
JOAOGOMES22 <joaolock7@gmail.com>
2021-11-02 12:44:27 -0300

    exercicio 1

---
+++ aula11/ex1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#include <locale.h>
+
+float somar (float N1, float N2){
+    return N1 + N2;
+}
+
+float sub (float N1, float N2){
+    return N1 - N2;
+}
+
+float multi (float N1, float N2){
+    return N1 * N2;
+}
+
+float div (float N1, float N2){
+    return N1/N2;
+}
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int op;
+    float N1, N2, res;
+
+    printf("---- Calculadora Simples ----\n");
+    printf("-----------------------------");
+    printf("Primeiro valor: ");
+    scanf ("%f", &N1);
+    printf("Segundo valor: ");
+    scanf ("%f", &N2);
+    printf("-----------------------------\n");
+    printf("1- Somar \n");
+    printf("2- Subtrair \n");
+    printf("3- Multiplicar \n");
+    printf("4- Dividir \n\n");
+    printf("Faca sua escolha: ");
+    scanf ("%d", &op);
+        switch (op){
+        case 1:
+            res = somar(N1, N2);
+            break;
+        case 2:
+            res = sub(N1, N2);
+            break;
+        case 3:
+            res = multi(N1, N2);
+            break;
+        case 4:
+            if (N2 == 0){
+                printf("Nao eh possivel dividir por 0");
+            }else{
+                res = div(N1, N2);
+            }
+            break;
+        default:
+            printf("Opção invalida!!");
+            break; 
+        }
+    printf("-----------------------------\n");
+    printf("Resultado %.2f\n\n",res);
+    system("pause");
+
+return 0;
+}
